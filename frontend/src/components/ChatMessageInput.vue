163215cf16a603caab8b61399e0d8db2cf06da24
39340 <iacopo.cutino@gmail.com>
2026-03-03 17:10:18 +0100

    bozza logica input text chat.

--- frontend/src/components/ChatMessageInput.vue
+++ frontend/src/components/ChatMessageInput.vue
@@ -1,9 +1,48 @@
 <script setup>
+import {ref} from "vue";
+
+const messages = ref([]);
+const messagesInput = ref("");
+const loading = ref(false);
+
+async function handleSubmit() {
+  if (!messagesInput.value) return;
+
+  messages.value.push({
+    role: "user",
+    content: messagesInput.value,
+  });
+
+  loading.value = true;
+
+  const response = await fetch('api/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      messages: messages.value,
+    }),
+  });
+
+  const data = await response.json();
+
+  messages.value.push(data.reply);
+  messagesInput.value = '';
+  loading.value = false;
+}
 </script>
 
 <template>
   <h3>Chat Message</h3>
-  <label for="message">Message:</label>
-  <input type="text" id="chat-message" name="message">
-  <input type="submit" value="Send">
+  <div v-for="(message, index) in messages" :key="index">
+    <p>{{ message.role }}: {{ message.content }}</p>
+  </div>
+
+  <form @submit.prevent="handleSubmit">
+  <input type="text" id="chat-message" v-model="messagesInput" name="message" placeholder="Write a message...">
+  <button type="submit">Send</button>
+  </form>
+
+  <p v-if="loading">I'm thinking...</p>
 </template>
