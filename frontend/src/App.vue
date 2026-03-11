c9675bba56535a25cf322c5965e74dcd8c1a7ed7
39340 <iacopo.cutino@gmail.com>
2026-03-11 17:36:45 +0100

    aggiunti bozza colori griglia.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,10 @@ import HeaderMain from './components/HeaderMain.vue'
 </script>
 
 <template>
+  <div class="left-sidebar-grid">
 <HeaderMain/>
   <RouterView />
+  </div>
 </template>
 
 <style scoped>
