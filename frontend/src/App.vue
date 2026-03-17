ac81931b54d7a85f82d93f6313529b19f90c6073
39340 <iacopo.cutino@gmail.com>
2026-03-17 16:28:41 +0100

    aggiunto footer, modifica colori.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderMain from './components/HeaderMain.vue'
+import FooterMain from './components/FooterMain.vue'
 </script>
 
 <template>
   <div class="left-sidebar-grid">
 <HeaderMain/>
   <RouterView />
+    <FooterMain/>
   </div>
 </template>
 
---
+++ frontend/src/components/FooterMain.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const currentYear = ref(new Date().getFullYear());
+</script>
+
+<template>
+  <footer class="footer">
+    <p>{{ currentYear }} - Ask me</p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
