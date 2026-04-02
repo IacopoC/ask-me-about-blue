83b95596e414d3a1744021ef3c65a98d7c600212
39340 <iacopo.cutino@gmail.com>
2026-04-02 17:27:21 +0200

    aggiunto link openai.

--- frontend/src/components/FooterMain.vue
+++ frontend/src/components/FooterMain.vue
@@ -5,11 +5,15 @@ const currentYear = ref(new Date().getFullYear());
 
 <template>
   <footer class="footer">
-    <p>{{ currentYear }} - Ask me about Blue</p>
+    <p>{{ currentYear }} | Ask me about Blue - Created using <a class="white-link" href="https://openai.com/it-IT/api/">Openai API</a></p>
   </footer>
 </template>
 
 <style scoped>
+.white-link {
+  color: white;
+}
+
 .footer {
   padding: 1% 3%
 }
