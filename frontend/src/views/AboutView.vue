96de0c524a5c78b40afc06814db94e7666c4e259
39340 <iacopo.cutino@gmail.com>
2026-04-11 10:48:09 +0200

    modifica testo.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <main class="main-content">
   <div class="about">
     <h2 class="title-page">About this project</h2>
-    <p>This is a simple web project created to use the openai api and experiment.</p>
+    <p>This is a simple web project created to use the openai api and experiment with the communication between backend and interface. The answers will be about blue color and all there is about blue.</p>
   </div>
   </main>
 </template>
