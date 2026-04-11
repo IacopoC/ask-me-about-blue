64b2cbf3a5fa2fd1dd66695dd96a8a4e68b80c3d
39340 <iacopo.cutino@gmail.com>
2026-04-11 10:56:47 +0200

    correzione testo.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,7 +2,8 @@
   <main class="main-content">
   <div class="about">
     <h2 class="title-page">About this project</h2>
-    <p>This is a simple web project created to use the openai api and experiment with the communication between backend and interface. The answers will be about blue color and all there is about blue.</p>
+    <p>This is a simple web project created to use the openai api and experiment with the communication between backend and interface.</p>
+    <p>The answers will be about blue color and all there is about blue.</p>
   </div>
   </main>
 </template>
