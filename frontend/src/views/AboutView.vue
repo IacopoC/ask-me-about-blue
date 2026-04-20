0319f099341be811e7232793b9c55159a5fe99b3
39340 <iacopo.cutino@gmail.com>
2026-04-20 20:47:57 +0200

    aggiunta immagine balloon e testo.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,12 +1,30 @@
+<script setup>
+import AskmeBalloonImage from '@/assets/img/ask-me-blue-balloon.jpg';
+</script>
 <template>
   <main class="main-content">
   <div class="about">
     <h2 class="title-page">About this project</h2>
-    <p>This is a simple web project created to use the openai api and experiment with the communication between backend and interface.</p>
-    <p>The answers will be about blue color and all there is about blue.</p>
+    <p>A poem about Blue:</p>
+    <p class="p-top"><em>"Blue is the vast, quiet expanse of the sky,
+    A deep ocean mirror where the lazy clouds lie.
+    From cerulean serenity to navy’s deep dream,
+    It’s the calm of the brook and the rush of the stream.
+    It is tranquil, a sapphire, a soft evening shade,
+    Where memories rest and peaceful thoughts are made."</em></p>
+    <p class="p-top">The answers will be about blue color and all there is about blue...</p>
+    <img :src="AskmeBalloonImage" alt="AskmeBalloon" class="askme-balloon" />
   </div>
   </main>
 </template>
 
 <style>
+.p-top {
+  padding: 10px 0 0 0;
+}
+
+.askme-balloon {
+  padding: 10px 0;
+  width: 100%;
+}
 </style>
